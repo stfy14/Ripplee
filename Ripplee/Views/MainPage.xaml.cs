405cfcba76deaa6666a9df41a6cbf2c8e9933b53
stfy14 <teamft2005@gmail.com>
2025-02-05 17:22:09 +0500

    UI Update

--- Ripplee/Views/MainPage.xaml.cs
+++ Ripplee/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Ripplee.Views;
+﻿using CommunityToolkit.Maui.Views;
+
+namespace Ripplee.Views;
 
 public partial class MainPage : ContentPage
 {
@@ -6,5 +8,18 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
         BindingContext = new ViewModels.MainViewModel();
+    }
+    private async void OnMenuButtonClicked(object sender, EventArgs e)
+    {
+        if (menuControl.IsMenuOpen) 
+        {
+            await menuControl.ToggleMenuAsync();
+            await menuButton.RotateTo(0, 300); 
+        }
+        else
+        {
+            await menuControl.ToggleMenuAsync();
+            await menuButton.RotateTo(90, 300); 
+        }
     }
 }
