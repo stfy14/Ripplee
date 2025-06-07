85b9def6b2cb5c560ac85e131f55d5cd58554c26
stfy <teamft2005@gmail.com>
2025-06-07 05:06:45 +0500

    last commit before refactoring

---
+++ Ripplee/Misc/UI/MenuMessages.cs
@@ -0,0 +1,6 @@
+﻿// Misc/UI/MenuMessages.cs
+namespace Ripplee.Misc.UI
+{
+    public class CloseMenuMessage { }
+    public class OpenMenuMessage { }
+}
---
+++ Ripplee/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,40 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Ripplee.ViewModels
+{
+    public partial class SettingsViewModel : ObservableObject
+    {
+        [RelayCommand]
+        private void ChangePhoto()
+        {
+            // Реализация изменения фото
+        }
+
+        [RelayCommand]
+        private void ChangePassword()
+        {
+            // Реализация изменения пароля
+        }
+
+        [RelayCommand]
+        private void ChangeEmail()
+        {
+            // Реализация изменения email
+        }
+
+        [RelayCommand]
+        private async Task DeleteAccount()
+        {
+            bool confirm = await Shell.Current.DisplayAlert(
+                "Подтверждение", 
+                "Вы уверены, что хотите удалить аккаунт?", 
+                "Да", "Отмена");
+                
+            if (confirm)
+            {
+                // Реализация удаления аккаунта
+            }
+        }
+    }
+}
